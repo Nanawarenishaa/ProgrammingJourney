f278c3372e6d8e393583724d45dbe16b4e8acccd
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-02-05 06:47:48 -0800

    Add files via upload

---
+++ VB.net[form]/Form2/Form2.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form2
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Username As String = "admin"
+        Dim Password As String = "Pass@317"
+        If TextBox1.Text = Username And TextBox2.Text = Password Then
+            MessageBox.Show("Login Successful!")
+            Me.Hide()
+            Dim Dashbord As New Form3
+            Dashbord.Show()
+        Else
+            MessageBox.Show("Login Failed!")
+        End If
+    End Sub
+End Class
---
+++ VB.net[form]/Form4/Form4.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form4
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(TextBox1.Text)
+        TextBox1.Clear()
+
+    End Sub
+    Private Sub ListBox1_SelectIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        MessageBox.Show(ListBox1.SelectedItems(0).ToString())
+    End Sub
+
+
+
+
+End Class
