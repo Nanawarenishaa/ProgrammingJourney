d9dfc4b22d8743a05551fdd2e8a712f8ffda4a9d
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-02-09 06:27:33 -0800

    Add files via upload

---
+++ VB.net[form]/ControlForms/CheckBox/Form2.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Choices As String = "You have selected: "
+        If CheckBox1.Checked Then Choices &= "option 1, "
+
+        If CheckBox2.Checked Then Choices &= "option 2, "
+
+
+        If CheckBox3.Checked Then Choices &= "option 3, "
+
+        MessageBox.Show(Choices.TrimEnd(","c, " "c))
+
+    End Sub
+End Class
---
+++ VB.net[form]/ControlForms/Label/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Label1_Load(sender As Object, e As EventArgs) Handles Label1.Click
+
+        Label1.Text = "welcom to forms Controls!"
+        Label1.Font = New Font("Arial", 12, FontStyle.Bold)
+        Label1.ForeColor = Color.Blue
+    End Sub
+
+End Class
